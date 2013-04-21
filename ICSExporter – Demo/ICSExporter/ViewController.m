c5d5ea495e62dfcbae89dab28ffd0ee3ecaa4c01
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2013-04-21 21:28:09 +0100

    Commented algorithms used in ViewController.m
    These two algorithms – one for loading all events for the month, one to
    load events for a particular day – appear to be the most efficient
    approach.

--- ICSExporter – Demo/ICSExporter/ViewController.m
+++ ICSExporter – Demo/ICSExporter/ViewController.m
@@ -17,25 +17,39 @@
 
 @implementation ViewController
 
--(void)calendarView:(VRGCalendarView *)calendarView switchedToMonth:(int)month year:(int)year numOfDays:(int)days targetHeight:(float)targetHeight animated:(BOOL)animated {    
+-(void)calendarView:(VRGCalendarView *)calendarView switchedToMonth:(int)month year:(int)year numOfDays:(int)days targetHeight:(float)targetHeight animated:(BOOL)animated {
+    // If this month hasn't already loaded and been cached, start loading events
     if (![[savedDates objectForKey:[NSNumber numberWithInt:year]] objectForKey:[NSNumber numberWithInt:month]]) {
+        
+        // Show a loading HUD (https://github.com/jdg/MBProgressHUD)
         MBProgressHUD *loadingHUD = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
         [loadingHUD setMode:MBProgressHUDModeIndeterminate];
         [loadingHUD setLabelText:@"Loading..."];
         
+        // Check the month on a background thread
         dispatch_async( dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            
             NSMutableArray *daysArray = [[NSMutableArray alloc] init];
             
+            // Create a formatter to provide the date
             NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
             [dateFormatter setDateFormat:@"yyyyddMM"];
             
+            // For this initial check, all we need to know is whether there's at least ONE event on each day, nothing more.
+            // So we loop through each event...
             for (MXLCalendarEvent *event in currentCalendar.events) {
+                
                 NSDateComponents *components = [[NSCalendar currentCalendar] components:NSMonthCalendarUnit | NSDayCalendarUnit fromDate:[event eventStartDate]];
                 
+                // If the event starts this month, add it to the array
                 if ([components month] == month) {
                     [daysArray addObject:[NSNumber numberWithInteger:[components day]]];
                     [currentCalendar addEvent:event onDateString:[dateFormatter stringFromDate:[event eventStartDate]]];
                 } else {
+                    // We loop through each day, check if there's an event already there
+                    // and if there is, we move onto the next one and repeat until we find a day WITHOUT an event on.
+                    // Then we check if this current event occurs then.
+                    // This is a way of reducing the number of checkDate: runs we need to do. It also means the algorithm speeds up as it progresses
                     for (int i = 1; i <= days; i++) {
                         if (![daysArray containsObject:[NSNumber numberWithInt:i]]) {
                             if ([event checkDay:i month:month year:year]) {
@@ -47,44 +61,53 @@
                 }
             }
             
+            // Cache the events
             if (![savedDates objectForKey:[NSNumber numberWithInt:year]]) {
                 [savedDates setObject:[NSMutableDictionary dictionaryWithObject:@[] forKey:[NSNumber numberWithInt:month]] forKey:[NSNumber numberWithInt:year]];
             }
-
             [[savedDates objectForKey:[NSNumber numberWithInt:year]] setObject:daysArray forKey:[NSNumber numberWithInt:month]];
 
+            // Refresh the UI on main thread
             dispatch_async( dispatch_get_main_queue(), ^{
                 [calendarView markDates:daysArray];
                 [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
             });
         });
     } else {
+        // If it's already cached, we're done
         [calendarView markDates:[[savedDates objectForKey:[NSNumber numberWithInt:year]] objectForKey:[NSNumber numberWithInt:month]]];    
     }
-    
-    
 }
 
--(void)calendarView:(VRGCalendarView *)calendarView dateSelected:(NSDate *)date {    
+-(void)calendarView:(VRGCalendarView *)calendarView dateSelected:(NSDate *)date {
+    // Check if all the events on this day have loaded
     if (![currentCalendar hasLoadedAllEventsForDate:date]) {
+        // If not, show a loading HUD
         MBProgressHUD *progressHUD = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
         [progressHUD setMode:MBProgressHUDModeIndeterminate];
         [progressHUD setLabelText:@"Loading..."];
     }
     
+    // Run on a background thread
     dispatch_async( dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         
+        // If the day hasn't already loaded events...
         if (![currentCalendar hasLoadedAllEventsForDate:date]) {
+            // Loop through each event and check whether it occurs on the selected date
             for (MXLCalendarEvent *event in currentCalendar.events) {
+                // If it does, save it for the date
                 if ([event checkDate:date]) {
                     [currentCalendar addEvent:event onDate:date];
                 }
             }
+            // Set that the calendar HAS loaded all the events for today
             [currentCalendar loadedAllEventsForDate:date];
         }
         
+        // load up the events for today
         currentEvents = [currentCalendar eventsForDate:date];
         
+        // Refresh UI
         dispatch_async(dispatch_get_main_queue(), ^{
             [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
             
